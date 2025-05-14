ffb0325cca413b744f534ae654b7941378f27ff1
JananiArunachalam03 <jananiarunachalam03@gmail.com>
2025-05-14 17:44:42 +0530

    feat : Logout buttons added in admin and home pages

--- client/src/Admin/Add.tsx
+++ client/src/Admin/Add.tsx
@@ -1,16 +1,28 @@
 import NFLogo from "../assets/netflix-logo.png";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Movie from "../Admin/AddMovie";
 import Series from "../Admin/AddSeries";
 
 const Add = () => {
+  const navigate = useNavigate();
   const [contentType, setContentType] = useState("movie");
-
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken"); // Clear stored token
+    localStorage.removeItem("isAdmin"); // Clear admin status
+    navigate("/login"); // Redirect to login page
+  };
   return (
     <>
-    <div className="pt-15 pl-20 text-white">
-      <img src={NFLogo} alt="Netflix Logo" className="w-36 " />
-    </div>
+    <div className="flex justify-between items-center p-5 text-white">
+        <img src={NFLogo} alt="Netflix Logo" className="w-36" />
+        <button
+          onClick={handleLogout}
+          className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded transition font-bold"
+        >
+          Logout
+        </button>
+      </div>
       <div className="flex flex-col justify-center ml-10 items-center">
         <p className="text-lg font-semibold">Select Content Type</p>
         <select value={contentType} onChange={(e) => setContentType(e.target.value)} className="bg-gray-800 px-4 py-2 rounded mt-2">
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import NFlogo from "../assets/netflix-logo.png";
 const Home = () => {
   const navigate = useNavigate();
   console.log("Home")
@@ -11,9 +11,14 @@ const Home = () => {
   };
 
   return (
-    <div>
-      
-    </div>
+      <div className="flex justify-between items-center p-5 text-white">
+        <button
+          onClick={handleLogout}
+          className="bg-red-600 hover:bg-red-500 text-white px-4 py-2 rounded transition font-bold"
+        >
+          Logout
+        </button>
+      </div>
   );
 };
 
